eb71a32bdac63132cfdac35d6edea3ecd00ba29f
John Hewitt <john.hewitt64@gmail.com>
2017-01-21 03:57:06 -0500

    add functioning wiki-learned phrase extractor

--- extract_phrases.py
+++ extract_phrases.py
@@ -9,7 +9,7 @@ import codecs
 from collections import Counter
 import argparse
 
-LAMBDA = 1
+LAMBDA = 0
 
 '''
 Takes a list of sentences (each of which is a list of words.)
@@ -18,6 +18,7 @@ Generates ngrams for the sentences.
 '''
 def ngrams_of_sents(sents, k):
     for sent in sents:
+        sent = [x.strip() for x in sent.split()]
         for ngram in get_ngrams(sent, k):
             yield ngram
     
@@ -45,6 +46,16 @@ def get_counter(sentence_list):
         word_counter.update(sentence)
     return word_counter
 
+
+def get_stats(fileobj):
+    y = Counter()
+    for line in fileobj:
+        line = filter(lambda z: bool(z), [x.strip() for x in line.split()])
+        y.update(line)
+    return y
+
+
+
 '''
 Takes a list of sentences (each of which is a list of words.)
 
@@ -52,16 +63,20 @@ Returns the ratios of observed cooccurence statistics between pairs of words
 and the hypothetical ratio of cooccurence if the words appeared independently
 of each other.
 '''
-def ratios(sentence_list):
-    print >> sys.stderr, 'size of sentence list', len(sentence_list)
-    y = get_counter(sentence_list)
+def ratios(filename):
+    #print >> sys.stderr, 'size of sentence list', len(sentence_list)
+    #y = get_counter(sentence_list)
+    print >> sys.stderr, "compiling statistics..."
+    y = get_stats(codecs.open(filename, 'r', 'utf-8'))
+    print >> sys.stderr, "done"
     n = float(sum(y.values()))
     pi = {w:y[w]/n for w in y}
 
     ratio_dict = {}
 
-    ngrams = [x for x in ngrams_of_sents(sentence_list,2)]
-    print >> sys.stderr, 'number of ngrams', len(ngrams)
+    print >> sys.stderr, "extracting 2grams..."
+    ngrams = [x for x in ngrams_of_sents(codecs.open(filename, 'r', 'utf-8'),2)]
+    print >> sys.stderr, 'done'
     y_ngrams = Counter(ngrams)
     n_ngrams = float(sum(y_ngrams.values()))
     pi_ngrams = {w:y_ngrams[w]/n_ngrams for w in y_ngrams}
@@ -74,15 +89,17 @@ def ratios(sentence_list):
 
 
     filtered_dict = {}
+    print >> sys.stderr, 'printing filtered phrases...'
     for (wordone, wordtwo) in ratio_dict:
         if wordone.isnumeric() or wordtwo.isnumeric():
             continue
-        #if (y[wordone] > LAMBDA or wordone.istitle()) \
-        #and (y[wordtwo] > LAMBDA or wordtwo.istitle()):
-        if (y[wordone] > LAMBDA)\
-                and (y[wordtwo] > LAMBDA):
+        if (y[wordone] > LAMBDA or wordone.istitle()) \
+        and (y[wordtwo] > LAMBDA or wordtwo.istitle()):
+        #if (y[wordone] > LAMBDA)\
+        #        and (y[wordtwo] > LAMBDA):
             filtered_dict[(wordone, wordtwo)] = ratio_dict[(wordone, wordtwo)]
 
+    print >> sys.stderr, 'done...'
     return filtered_dict 
     
 
@@ -96,7 +113,8 @@ class Phrasifier():
     phrases = set()
 
     def __init__(self, filepath):
-        for line in csv.reader(open(filepath), delimiter='\t'):
+        for line in codecs.open(filepath, 'r', 'utf-8'):
+            line = [x.strip() for x in line.split('\t')]
             self.phrases.add((line[0], line[1]))
 
     '''
@@ -108,7 +126,6 @@ class Phrasifier():
             buf = []
             for word in sent:
                 if len(buf) == 2:
-                    print buf
                     if tuple(buf) in self.phrases:
                         words = tuple(buf)
                         buf = [word]
@@ -152,9 +169,9 @@ if __name__ == '__main__':
     argp = argparse.ArgumentParser()
     argp.add_argument('corpus')
     args = argp.parse_args()
-    corpus = codecs.open(args.corpus, 'r', 'utf-8').readlines()
-    corpus = [filter(lambda z: bool(z), [x.strip() for x in y.split()]) for y in corpus]
-    ratio_list = ratios(corpus)
+    #corpus = codecs.open(args.corpus, 'r', 'utf-8').readlines()
+    #corpus = [filter(lambda z: bool(z), [x.strip() for x in y.split()]) for y in corpus]
+    ratio_list = ratios(args.corpus)
     for word1, word2, in sorted(ratio_list, key = lambda x: ratio_list[x]):
         print '\t'.join([word1.encode('utf-8'), word2.encode('utf-8'), str(ratio_list[(word1, word2)])])
 
