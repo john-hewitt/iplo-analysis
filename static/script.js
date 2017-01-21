afb39bae93e9b417b768eb6e8f9c8968d3b97cb8
Henry Zhu <henryzhu@seas.upenn.edu>
2017-01-21 16:19:05 -0500

    added some changes

--- static/script.js
+++ static/script.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
     $("").hide();
 
+    //SetupTitle();
     SetupTextBoxes();
-	$("div.submit-button").focus(function() {
-		$(this).css("background-color", "blue");
-	});
+    SetupAnimation();
 });
+
+function SetupTitle() {
+   $(".left-input-textarea").on({
+   		focus: function() {
+   			
+   		},
+	    mouseenter: function(){
+	        $(this).animate({height: '500px'}, "slow");
+	        $(".right-input-textarea").animate({height: '500px'}, "slow");
+	    }, 
+	    mouseleave: function(){
+	        $(this).animate({height: '100px'}, "slow");
+	        $(".right-input-textarea").animate({height: '100px'}, "slow");
+	    }, 
+	    click: function(){
+	        
+	    } 
+	});
+}
+
 
 function SetupTextBoxes() {
    $(".left-input-textarea").on({
@@ -40,4 +59,36 @@ function SetupTextBoxes() {
 	        
 	    } 
 	});
+}
+
+function SetupAnimation() {
+	var $animation_elements = $('.textarea-animation-element');
+	var $window = $(window);
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+	 
+	  $.each($animation_elements, function() {
+	    var $element = $(this);
+	    var element_height = $element.outerHeight();
+	    var element_top_position = $element.offset().top;
+	    var element_bottom_position = (element_top_position + element_height);
+
+	    //check to see if this current container is within viewport
+	    if ((element_bottom_position >= window_top_position) &&
+	        (element_top_position <= window_bottom_position)) {
+	        $element.fadeIn( 2000, function() {
+			    // Animation complete
+			});
+	    } else {
+	    	$element.fadeOut( 2000, function() {
+			    // Animation complete
+			});
+	    }
+	  });
+	}
 }
