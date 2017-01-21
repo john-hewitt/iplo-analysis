c8ac086233d80afbcb76b3c737485ded1fa0caf5
robinz62 <robinz672@gmail.com>
2017-01-21 02:06:40 -0500

    added function for tweets

--- custom_tokenize.py
+++ custom_tokenize.py
@@ -1,4 +1,5 @@
 from nltk.tokenize import sent_tokenize, regexp_tokenize
+from nltk.tokenize import casual
 
 # sent_tokenize is an nltk function that splits the input string into a list of sentences based
 # on an some machine learning that smart people did
@@ -12,3 +13,8 @@ def generate_tokens(text1, text2):
 	tokens1 = [regexp_tokenize(t, '\w+\'*\w*') for t in sent_tokenize(text1)]
 	tokens2 = [regexp_tokenize(t, '\w+\'*\w*') for t in sent_tokenize(text2)]
 	return (tokens1, tokens2)
+
+def generate_tokens_twitter(text1, text2):
+	text1 = casual.remove_handles(text1)
+	text2 = casual.remove_handles(text2)
+	return generate_tokens(text1, text2)
