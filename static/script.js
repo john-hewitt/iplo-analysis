e90638ae030f3452cefb1caa06f6de5fc3c4ff63
robinz62 <robinz672@gmail.com>
2017-01-21 13:23:00 -0500

    added some animations

--- static/script.js
+++ static/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     $("").hide();
 
-    SetupTextBoxes();
+    /* SetupTextBoxes();*/
 	$("div.submit-button").focus(function() {
 		$(this).css("background-color", "blue");
 	});
 });
 
+/*
 function SetupTextBoxes() {
    $(".area-input").on({
    		focus: function() {
@@ -23,3 +24,16 @@ function SetupTextBoxes() {
 	    } 
 	});
 }
+*/
+
+$(':radio').change(function() {
+	if (this.value == "plaintext") {
+		$('#twitter').fadeOut(function() {
+			$('#plain').fadeIn();
+		});
+	} else if (this.value == "twitter") {
+		$('#plain').fadeOut(function() {
+			$('#twitter').fadeIn();
+		});
+	}
+});
