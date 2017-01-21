18badb5a6e7e00029d4524636c8f7ca3a7e6fb86
Henry Zhu <henryzhu@seas.upenn.edu>
2017-01-21 06:06:02 -0500

    Added some cool effects, scrolling, etc

--- static/script.js
+++ static/script.js
@@ -8,15 +8,33 @@ $(document).ready(function() {
 });
 
 function SetupTextBoxes() {
-   $(".area-input").on({
+   $(".left-input-textarea").on({
    		focus: function() {
-   			$(this).css("background-color", "lightgreen");
-   		}
+   			
+   		},
 	    mouseenter: function(){
-	        
+	        $(this).animate({height: '500px'}, "slow");
+	        $(".right-input-textarea").animate({height: '500px'}, "slow");
 	    }, 
 	    mouseleave: function(){
+	        $(this).animate({height: '100px'}, "slow");
+	        $(".right-input-textarea").animate({height: '100px'}, "slow");
+	    }, 
+	    click: function(){
 	        
+	    } 
+	});
+    $(".right-input-textarea").on({
+   		focus: function() {
+   			
+   		},
+	    mouseenter: function(){
+	        $(this).animate({height: '500px'}, "slow");
+	        $(".left-input-textarea").animate({height: '500px'}, "slow");
+	    }, 
+	    mouseleave: function(){
+	        $(this).animate({height: '100px'}, "slow");
+	        $(".left-input-textarea").animate({height: '100px'}, "slow");
 	    }, 
 	    click: function(){
 	        
