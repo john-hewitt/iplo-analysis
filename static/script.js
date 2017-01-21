db63935c01ea043945a5a77cff19550208102b98
Henry Zhu <henryzhu@seas.upenn.edu>
2017-01-21 18:05:57 -0500

    added textiallate

--- static/script.js
+++ static/script.js
@@ -1,26 +1,64 @@
 $(document).ready(function() {
-    //SetupTitle();
+    SetupTitle();
     SetupTextBoxes();
     //SetupAnimation();
 });
 
 function SetupTitle() {
-   $(".left-input-textarea").on({
-   		focus: function() {
-   			
-   		},
-	    mouseenter: function(){
-	        $(this).animate({height: '500px'}, "slow");
-	        $(".right-input-textarea").animate({height: '500px'}, "slow");
-	    }, 
-	    mouseleave: function(){
-	        $(this).animate({height: '100px'}, "slow");
-	        $(".right-input-textarea").animate({height: '100px'}, "slow");
-	    }, 
-	    click: function(){
-	        
-	    } 
-	});
+   $('.title').textillate({
+  // the default selector to use when detecting multiple texts to animate
+  selector: '.texts',
+  
+  // enable looping
+  loop: true,
+  
+  // sets the minimum display time for each text before it is replaced
+  minDisplayTime: 1500,
+  
+  // sets the initial delay before starting the animation
+  // (note that depending on the in effect you may need to manually apply 
+  // visibility: hidden to the element before running this plugin)
+  initialDelay: 0,
+  
+  // set whether or not to automatically start animating
+  autoStart: true,
+  
+  // custom set of 'in' effects. This effects whether or not the 
+  // character is shown/hidden before or after an animation  
+  inEffects: [],
+  
+  // custom set of 'out' effects
+  outEffects: [ 'rotateInUpRight' ],
+  
+  // in animation settings
+  in: {
+  // set the effect name
+  effect: 'flash',
+  
+  // set the delay factor applied to each consecutive character
+  delayScale: 1.5,
+  
+  // set the delay between each character
+  delay: 50,
+  
+  // set to true to animate all the characters at the same time
+  sync: false,
+  
+  // randomize the character sequence 
+  // (note that shuffle doesn't make sense with sync = true)
+  shuffle: true
+},
+
+  // out animation settings.
+  out: {
+    effect: 'rotateInUpRight',
+    delayScale: 1.5,
+    delay: 50,
+    sync: false,
+    shuffle: true,
+  },
+})
+
 }
 
 function SetupTextBoxes() {
